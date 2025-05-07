01564828e4d14dd68b39c2b06bf27a161cd313ab
Dale Davies <dale.davies@catalyst-eu.net>
2025-05-07 15:01:10 +0100

    Fix settings siteconfig permission check

--- settings.php
+++ settings.php
@@ -29,93 +29,97 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$ADMIN->add(
-    'tools',
-    new admin_category('toolregistrationrules', new lang_string('pluginname', 'tool_registrationrules')),
-);
+// Settings should only be available for users with site config capability.
+if ($hassiteconfig) {
 
-// The main registrationrules plugin settings.
-$settings = new admin_settingpage(
-    'generalregistrationrulessettings',
-    new lang_string('settings:registrationrulessettings', 'tool_registrationrules'),
-    'moodle/site:config',
-);
-if ($ADMIN->fulltree) {
-    global $OUTPUT;
+    $ADMIN->add(
+        'tools',
+        new admin_category('toolregistrationrules', new lang_string('pluginname', 'tool_registrationrules')),
+    );
 
-    // Show guidance for how to begin with registrationrules.
-    $setting = new admin_setting_description(
-        'tool_registrationrules/guidancemessage',
-        null,
-        $OUTPUT->notification(get_string('settings:guidancemessage', 'tool_registrationrules'), 'success', false)
+    // The main registrationrules plugin settings.
+    $settings = new admin_settingpage(
+        'generalregistrationrulessettings',
+        new lang_string('settings:registrationrulessettings', 'tool_registrationrules'),
+        'moodle/site:config',
     );
-    $settings->add($setting);
+    if ($ADMIN->fulltree) {
+        global $OUTPUT;
 
-    // Enable/disable.
-    $settings->add(new admin_setting_configcheckbox(
-        'tool_registrationrules/enable',
-        new lang_string('enable', 'core'),
-        new lang_string('settings:enable:description', 'tool_registrationrules'),
-        0,
-    ));
-    // Max points.
-    $setting = new admin_setting_configtext(
-        'tool_registrationrules/maxpoints',
-        new lang_string('settings:maxpoints', 'tool_registrationrules'),
-        new lang_string('settings:maxpoints:description', 'tool_registrationrules'),
-        100,
-        PARAM_INT,
-    );
-    $settings->add($setting);
-    // Generic message for registration page.
-    $setting = new admin_setting_confightmleditor(
-        'tool_registrationrules/registrationpagemessage',
-        new lang_string('settings:registrationpagemessage', 'tool_registrationrules'),
-        new lang_string('settings:registrationpagemessage:description', 'tool_registrationrules'),
-        '',
-    );
-    $settings->add($setting);
-    // General message to show on the error page before any rule specific messages.
-    $setting = new admin_setting_confightmleditor(
-        'tool_registrationrules/generalbeforemessage',
-        new lang_string('settings:generalbeforemessage', 'tool_registrationrules'),
-        new lang_string('settings:generalbeforemessage:description', 'tool_registrationrules'),
-        '',
-    );
-    $settings->add($setting);
-    // Only log information, do not actually deny registration.
-    $settings->add(
-        new admin_setting_configcheckbox(
-            'tool_registrationrules/loggingonly',
-            new lang_string('settings:loggingonly', 'tool_registrationrules'),
-            new lang_string('settings:loggingonly:description', 'tool_registrationrules'),
+        // Show guidance for how to begin with registrationrules.
+        $setting = new admin_setting_description(
+            'tool_registrationrules/guidancemessage',
+            null,
+            $OUTPUT->notification(get_string('settings:guidancemessage', 'tool_registrationrules'), 'success', false)
+        );
+        $settings->add($setting);
+
+        // Enable/disable.
+        $settings->add(new admin_setting_configcheckbox(
+            'tool_registrationrules/enable',
+            new lang_string('enable', 'core'),
+            new lang_string('settings:enable:description', 'tool_registrationrules'),
             0,
-        ),
+        ));
+        // Max points.
+        $setting = new admin_setting_configtext(
+            'tool_registrationrules/maxpoints',
+            new lang_string('settings:maxpoints', 'tool_registrationrules'),
+            new lang_string('settings:maxpoints:description', 'tool_registrationrules'),
+            100,
+            PARAM_INT,
+        );
+        $settings->add($setting);
+        // Generic message for registration page.
+        $setting = new admin_setting_confightmleditor(
+            'tool_registrationrules/registrationpagemessage',
+            new lang_string('settings:registrationpagemessage', 'tool_registrationrules'),
+            new lang_string('settings:registrationpagemessage:description', 'tool_registrationrules'),
+            '',
+        );
+        $settings->add($setting);
+        // General message to show on the error page before any rule specific messages.
+        $setting = new admin_setting_confightmleditor(
+            'tool_registrationrules/generalbeforemessage',
+            new lang_string('settings:generalbeforemessage', 'tool_registrationrules'),
+            new lang_string('settings:generalbeforemessage:description', 'tool_registrationrules'),
+            '',
+        );
+        $settings->add($setting);
+        // Only log information, do not actually deny registration.
+        $settings->add(
+            new admin_setting_configcheckbox(
+                'tool_registrationrules/loggingonly',
+                new lang_string('settings:loggingonly', 'tool_registrationrules'),
+                new lang_string('settings:loggingonly:description', 'tool_registrationrules'),
+                0,
+            ),
+        );
+    }
+    $ADMIN->add('toolregistrationrules', $settings);
+
+    // Link to registration rule subplugin management page.
+    $temp = new admin_settingpage('manageregistrationrules', get_string('manageregistrationruleplugins', 'tool_registrationrules'));
+    $temp->add(new tool_registrationrules\admin\manage_rule_plugins());
+    $ADMIN->add('toolregistrationrules', $temp);
+
+    // Link to rule instances management page.
+    $manageinstancespage = new admin_externalpage(
+        'toolregistrationrules_instances',
+        new lang_string('settings:registrationruleinstances', 'tool_registrationrules'),
+        new moodle_url('/admin/tool/registrationrules/manageruleinstances.php'),
+        'moodle/site:config',
     );
-}
-$ADMIN->add('toolregistrationrules', $settings);
+    $ADMIN->add('toolregistrationrules', $manageinstancespage);
 
-// Link to registration rule subplugin management page.
-$temp = new admin_settingpage('manageregistrationrules', get_string('manageregistrationruleplugins', 'tool_registrationrules'));
-$temp->add(new tool_registrationrules\admin\manage_rule_plugins());
-$ADMIN->add('toolregistrationrules', $temp);
-
-// Link to rule instances management page.
-$manageinstancespage = new admin_externalpage(
-    'toolregistrationrules_instances',
-    new lang_string('settings:registrationruleinstances', 'tool_registrationrules'),
-    new moodle_url('/admin/tool/registrationrules/manageruleinstances.php'),
-    'moodle/site:config',
-);
-$ADMIN->add('toolregistrationrules', $manageinstancespage);
-
-// Don't show settings for individual rule plugins in menu, but still show them in settings
-// category and on plugin management table.
-if ($ADMIN->fulltree) {
-    // Load the settings from registration rule subplugins and add a link to them.
-    $plugins = core_plugin_manager::instance()->get_plugins_of_type('registrationrule');
-    core_collator::asort_objects_by_property($plugins, 'displayname');
-    foreach ($plugins as $plugin) {
-        $plugin->load_settings($ADMIN, 'toolregistrationrules', $hassiteconfig);
+    // Don't show settings for individual rule plugins in menu, but still show them in settings
+    // category and on plugin management table.
+    if ($ADMIN->fulltree) {
+        // Load the settings from registration rule subplugins and add a link to them.
+        $plugins = core_plugin_manager::instance()->get_plugins_of_type('registrationrule');
+        core_collator::asort_objects_by_property($plugins, 'displayname');
+        foreach ($plugins as $plugin) {
+            $plugin->load_settings($ADMIN, 'toolregistrationrules', $hassiteconfig);
+        }
     }
 }
