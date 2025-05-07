86fb17ff107bc9359730df3455dd1110d9ab12ad
Dale Davies <dale.davies@catalyst-eu.net>
2025-05-07 15:46:41 +0100

    Issue #67: Fix cloudflare turnstile privacy provider namespace

--- rules/cloudflareturnstile/classes/privacy/provider.php
+++ rules/cloudflareturnstile/classes/privacy/provider.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace registrationrule_turnstile\privacy;
+namespace registrationrule_cloudflareturnstile\privacy;
 
 use coding_exception;
 use core_privacy\local\metadata\collection;
